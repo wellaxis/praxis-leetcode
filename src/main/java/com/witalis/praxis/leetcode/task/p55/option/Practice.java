8c4ec771820f54596926d9e34e54cecd03178072
wellaxis <wellaxis@gmail.com>
2021-12-06 12:28:27 +0200

    leetcode task #55

---
+++ src/main/java/com/witalis/praxis/leetcode/task/p55/option/Practice.java
@@ -0,0 +1,40 @@
+package com.witalis.praxis.leetcode.task.p55.option;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+
+/**
+ * ID: 55
+ * Name: Jump Game
+ * URL: https://leetcode.com/problems/jump-game/
+ * Note: try to find the better solution (without timing)
+ */
+@Slf4j
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Practice {
+    private int[] numbers;
+
+    public Boolean process() {
+        return canJump(numbers);
+    }
+
+    public boolean canJump(int[] nums) {
+        if (nums == null || nums.length == 0) return false;
+        if (nums.length == 1) return true;
+
+        int max = nums[0];
+        for (int i = 0; i < nums.length - 1; i++) {
+            // check, if it is possible to jump over zero
+            if (nums[i] == 0 && max <= i) return false;
+            // recalculate current max
+            max = Math.max(max, i + nums[i]);
+            // check, if it is possible to reach the tail
+            if (max >= nums.length - 1) return true;
+        }
+        return false;
+    }
+}
