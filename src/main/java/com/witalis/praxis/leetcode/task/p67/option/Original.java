3f52798ae16798ef8ab0aec7fecd790ea878e927
wellaxis <wellaxis@gmail.com>
2021-12-07 20:50:05 +0200

    leetcode task #67

---
+++ src/main/java/com/witalis/praxis/leetcode/task/p67/option/Original.java
@@ -0,0 +1,56 @@
+package com.witalis.praxis.leetcode.task.p67.option;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+
+/**
+ * ID: 67
+ * Name: Add Binary
+ * URL: https://leetcode.com/problems/add-binary/
+ * Note: the real test solution (code writing with timing)
+ */
+@Slf4j
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Original {
+    private String binary1;
+    private String binary2;
+
+    public String process() {
+        return addBinary(binary1, binary2);
+    }
+
+    public String addBinary(String a, String b) {
+        if (a == null || a.length() == 0) return b;
+        if (b == null || b.length() == 0) return a;
+
+        var result = new StringBuilder();
+
+        a = new StringBuilder(a).reverse().toString();
+        b = new StringBuilder(b).reverse().toString();
+
+        int index = 0;
+        int offset = 0;
+        while (index < Math.max(a.length(), b.length())) {
+            int ai = (index < a.length()) ? a.charAt(index) - '0' : 0;
+            int bi = (index < b.length()) ? b.charAt(index) - '0' : 0;
+
+            int sum = ai + bi + offset;
+            offset = (sum > 1) ? 1 : 0;
+
+            if (sum % 2 == 0) {
+                result.append('0');
+            } else {
+                result.append('1');
+            }
+
+            index++;
+        }
+        if (offset == 1) result.append('1');
+
+        return result.reverse().toString();
+    }
+}
---
+++ src/main/java/com/witalis/praxis/leetcode/task/p67/readme.md
@@ -0,0 +1,27 @@
+# [LeetCode][leetcode] task # 67: [Add Binary][task]
+
+Description
+-----------
+
+> Given two binary strings `a` and `b`,
+> return _their sum as a binary string_.
+
+Example
+-------
+
+```sh
+Input: a = "1010", b = "1011"
+Output: "10101"
+```
+
+Solution
+--------
+
+| Task | Solution |
+| :------: | :------ |
+| 67 | [Add Binary][solution] |
+
+
+[leetcode]: <http://leetcode.com/>
+[task]: <https://leetcode.com/problems/add-binary/>
+[solution]: <https://github.com/wellaxis/witalis-jkit/blob/main/module/tasks/src/main/java/com/witalis/jkit/tasks/core/task/leetcode/p67/option/Practice.java>
