4a570e3fb0c7536f39a20d7e64ada038f70ed15d
wellaxis <wellaxis@gmail.com>
2021-12-11 23:18:02 +0200

    leetcode task #70

---
+++ src/main/java/com/witalis/praxis/leetcode/task/p70/ClimbingStairs.java
@@ -0,0 +1,68 @@
+package com.witalis.praxis.leetcode.task.p70;
+
+import com.witalis.praxis.leetcode.task.LeetCodeTask;
+import com.witalis.praxis.leetcode.task.p70.option.*;
+import com.witalis.praxis.leetcode.utils.LeetCode;
+import com.witalis.praxis.leetcode.utils.TaskRevision;
+
+import lombok.extern.slf4j.Slf4j;
+import java.util.concurrent.ThreadLocalRandom;
+
+@Slf4j
+@LeetCode(id = 70, description = "Climbing Stairs")
+public class ClimbingStairs extends LeetCodeTask<Integer> {
+    public static final int STEPS = 45;
+    private int number;
+
+    public static final String INFORMATION = """
+
+        You are climbing a staircase. It takes n steps to reach the top.
+
+        Each time you can either climb 1 or 2 steps.
+            In how many distinct ways can you climb to the top?
+
+        Example:
+            Input: n = 3
+            Output: 3
+            Explanation: There are three ways to climb to the top.
+                1. 1 step + 1 step + 1 step
+                2. 1 step + 2 steps
+                3. 2 steps + 1 step""";
+
+    public ClimbingStairs(int id, String description, TaskRevision revision) {
+        super(id, description, revision);
+        initialization();
+    }
+
+    private void initialization() {
+        this.number = ThreadLocalRandom.current().nextInt(1, STEPS + 1);
+
+        log.info("Number is '{}'", number);
+    }
+
+    @Override
+    protected String getInformation() {
+        return INFORMATION;
+    }
+
+    // time = 572 ms, memory limit exceeded
+    @Override
+    protected Integer original() {
+        var original = new Original(number);
+        return original.process();
+    }
+
+    // time = 475 ms
+    @Override
+    protected Integer practice() {
+        var practice = new Practice(number);
+        return practice.process();
+    }
+
+    // time = 463 ms
+    @Override
+    protected Integer solution() {
+        var solution = new Solution(number);
+        return solution.process();
+    }
+}
---
+++ src/main/java/com/witalis/praxis/leetcode/task/p70/readme.md
@@ -0,0 +1,33 @@
+# [LeetCode][leetcode] task # 70: [Climbing Stairs][task]
+
+Description
+-----------
+
+> You are climbing a staircase. It takes `n` steps to reach the top.
+> 
+> Each time you can either climb `1` or `2` steps.
+> In how many distinct ways can you climb to the top?
+
+Example
+-------
+
+```sh
+Input: n = 3
+Output: 3
+Explanation: There are three ways to climb to the top.
+             1. 1 step + 1 step + 1 step
+             2. 1 step + 2 steps
+             3. 2 steps + 1 step
+```
+
+Solution
+--------
+
+| Task | Solution |
+|:----:| :------ |
+|  70  | [Climbing Stairs][solution] |
+
+
+[leetcode]: <http://leetcode.com/>
+[task]: <https://leetcode.com/problems/climbing-stairs/>
+[solution]: <https://github.com/wellaxis/witalis-jkit/blob/main/module/tasks/src/main/java/com/witalis/jkit/tasks/core/task/leetcode/p70/option/Practice.java>
