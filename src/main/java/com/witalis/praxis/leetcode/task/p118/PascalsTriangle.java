b5eb9a699d39d8ff5703ab3da79fa192e931a865
wellaxis <wellaxis@gmail.com>
2021-12-16 14:04:35 +0200

    leetcode task #118

---
+++ src/main/java/com/witalis/praxis/leetcode/task/p118/PascalsTriangle.java
@@ -0,0 +1,77 @@
+package com.witalis.praxis.leetcode.task.p118;
+
+import com.witalis.praxis.leetcode.task.LeetCodeTask;
+import com.witalis.praxis.leetcode.task.p118.option.*;
+import com.witalis.praxis.leetcode.utils.LeetCode;
+import com.witalis.praxis.leetcode.utils.TaskRevision;
+
+import lombok.extern.slf4j.Slf4j;
+
+import java.util.List;
+import java.util.concurrent.ThreadLocalRandom;
+
+@Slf4j
+@LeetCode(id = 118, description = "Pascal's Triangle")
+public class PascalsTriangle extends LeetCodeTask<List<List<Integer>>> {
+    public static final int LEN = 30;
+    private int rows;
+
+    public static final String INFORMATION = """
+
+        Given an integer numRows,
+            return the first numRows of Pascal's triangle.
+
+        In Pascal's triangle, each number is the sum of
+            the two numbers directly above it as shown:
+            
+                    1
+                   / \\
+                  1   1
+                 / \\ / \\
+                1   2   1
+               / \\ / \\ / \\
+              1   3   3   1
+             / \\ / \\ / \\ / \\
+            1   4   6   4   1
+
+        Example:
+            Input: numRows = 5
+            Output: [[1],[1,1],[1,2,1],[1,3,3,1],[1,4,6,4,1]]""";
+
+    public PascalsTriangle(int id, String description, TaskRevision revision) {
+        super(id, description, revision);
+        initialization();
+    }
+
+    private void initialization() {
+        this.rows = ThreadLocalRandom.current().nextInt(1, LEN + 1);
+
+        log.info("Number of rows is '{}'", rows);
+    }
+
+    @Override
+    protected String getInformation() {
+        return INFORMATION;
+    }
+
+    // time = 566 ms
+    @Override
+    protected List<List<Integer>> original() {
+        var original = new Original(rows);
+        return original.process();
+    }
+
+    // time = 437 ms
+    @Override
+    protected List<List<Integer>> practice() {
+        var practice = new Practice(rows);
+        return practice.process();
+    }
+
+    // time = 514 ms
+    @Override
+    protected List<List<Integer>> solution() {
+        var solution = new Solution(rows);
+        return solution.process();
+    }
+}
---
+++ src/main/java/com/witalis/praxis/leetcode/task/p118/option/Practice.java
@@ -0,0 +1,54 @@
+package com.witalis.praxis.leetcode.task.p118.option;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+/**
+ * ID: 118
+ * Name: Pascal's Triangle
+ * URL: https://leetcode.com/problems/pascals-triangle/
+ * Note: try to find the better solution (without timing)
+ */
+@Slf4j
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Practice {
+    private int rows;
+
+    public List<List<Integer>> process() {
+        return generate(rows);
+    }
+
+    public List<List<Integer>> generate(int numRows) {
+        if (numRows <= 0) return Collections.emptyList();
+
+        List<List<Integer>> triangle = new ArrayList<>(numRows);
+
+        List<Integer> previous = new ArrayList<>(1);
+        previous.add(1);
+        triangle.add(previous);
+
+        for (int i = 2; i <= numRows; i++) {
+            List<Integer> row = new ArrayList<>();
+            row.add(1);
+
+            int counter = 1;
+            while (counter < previous.size()) {
+                row.add(previous.get(counter - 1) + previous.get(counter));
+                counter++;
+            }
+
+            row.add(1);
+            triangle.add(row);
+            previous = row;
+        }
+        return triangle;
+    }
+}
