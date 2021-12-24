15016c264d590f4be55a7e54e13324136386b9b3
wellaxis <wellaxis@gmail.com>
2021-12-24 23:11:08 +0200

    leetcode task #746

---
+++ src/main/java/com/witalis/praxis/leetcode/task/p746/option/Original.java
@@ -0,0 +1,44 @@
+package com.witalis.praxis.leetcode.task.p746.option;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+
+/**
+ * ID: 746
+ * Name: Min Cost Climbing Stairs
+ * URL: https://leetcode.com/problems/min-cost-climbing-stairs/
+ * Note: the real test solution (code writing with timing)
+ */
+@Slf4j
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Original {
+    private int[] cost;
+
+    public Integer process() {
+        return minCostClimbingStairs(cost);
+    }
+
+    public int minCostClimbingStairs(int[] cost) {
+        if (cost == null || cost.length == 0) return 0;
+        if (cost.length == 1) return cost[0];
+        if (cost.length == 2) return Math.min(cost[0], cost[1]);
+
+        int oneUp = cost[0];
+        int twoUp = cost[1];
+
+        int step = 2;
+        while (step < cost.length) {
+            int sum = cost[step] + Math.min(oneUp, twoUp);
+            oneUp = twoUp;
+            twoUp = sum;
+
+            step++;
+        }
+
+        return Math.min(oneUp, twoUp);
+    }
+}
