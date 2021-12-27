3bece0bba181fc18ba526a2ce92aa2865cdd024e
wellaxis <wellaxis@gmail.com>
2021-12-27 16:26:28 +0200

    leetcode task #101

---
+++ src/main/java/com/witalis/praxis/leetcode/task/p101/option/Original.java
@@ -0,0 +1,41 @@
+package com.witalis.praxis.leetcode.task.p101.option;
+
+import com.witalis.praxis.leetcode.task.p101.content.TreeNode;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+
+/**
+ * ID: 101
+ * Name: Symmetric Tree
+ * URL: https://leetcode.com/problems/symmetric-tree/
+ * Note: the real test solution (code writing with timing)
+ */
+@Slf4j
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Original {
+    private TreeNode root;
+
+    public Boolean process() {
+        return isSymmetric(root);
+    }
+
+    public boolean isSymmetric(TreeNode root) {
+        return recursiveIsSymmetric(root, root);
+    }
+
+    private boolean recursiveIsSymmetric(TreeNode n1, TreeNode n2) {
+        if (n1 == null && n2 == null) return true;
+
+        if (n1 != null && n2 != null && n1.val == n2.val) {
+            return recursiveIsSymmetric(n1.left, n2.right)
+                && recursiveIsSymmetric(n1.right, n2.left);
+        } else {
+            return false;
+        }
+    }
+}
---
+++ src/main/java/com/witalis/praxis/leetcode/task/p101/readme.md
@@ -0,0 +1,30 @@
+# [LeetCode][leetcode] task # 101: [Symmetric Tree][task]
+
+Description
+-----------
+
+> Given the `root` of a binary tree,
+> _check whether it is a mirror of itself_
+> (i.e., symmetric around its center).
+
+ Example
+-------
+
+![tree.png](image/tree.png)
+
+```sh
+Input: root = [1,2,2,3,4,4,3]
+Output: true
+```
+
+Solution
+--------
+
+| Task | Solution                   |
+|:----:|:---------------------------|
+| 101  | [Symmetric Tree][solution] |
+
+
+[leetcode]: <http://leetcode.com/>
+[task]: <https://leetcode.com/problems/symmetric-tree/>
+[solution]: <https://github.com/wellaxis/witalis-jkit/blob/main/module/tasks/src/main/java/com/witalis/jkit/tasks/core/task/leetcode/p101/option/Practice.java>
